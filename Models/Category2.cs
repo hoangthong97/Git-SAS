71bbad9bff5934e5f29077d62b59a28b0fee3d86
Hoangthong97 <Hoangthong97.th@gmail.com>
2021-08-26 17:00:23 +0700

    update class category

--- Models/Category2.cs
+++ Models/Category2.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace WebApplication1.Models
 {
-	public class Category
+	public class Category2
 	{
 		public int Id { get; set; }
 		public string Name { get; set; }
